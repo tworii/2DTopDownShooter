cbe69f6a84d0ea042db8471e909d6138e979b3c7
DanyeWestinator <dcarsten@uci,edu>
2018-10-31 22:25:47 -0700

    Added a zoom script

---
+++ 2DTopDownShooter/Assets/Scripts/DaneMovementScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaneMovementScript : MonoBehaviour {
+    [Header("Player Variables")]
+    public float PlayerX = 0;
+    public float PlayerY = 0;
+    public float movementSpeed = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2DTopDownShooter/Assets/Scripts/cameraZoom.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraZoom : MonoBehaviour {
+    public Camera m_OrthographicCamera;
+    public float Size = 5f; //how zoomed in the camera currently is
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKey("q"))
+        {
+            if (m_OrthographicCamera.orthographicSize == 5.0f)
+            {
+                m_OrthographicCamera.orthographicSize = 3.0f;
+            }
+            
+        }
+        else
+        {
+            m_OrthographicCamera.orthographicSize = 5.0f;
+        }
+    }
+}
