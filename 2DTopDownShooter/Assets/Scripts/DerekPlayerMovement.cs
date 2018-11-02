245dafb1e3cb04070eac9e0b7b3eb2cbefc82249
Derek Tran <dphtran501@gmail.com>
2018-11-02 16:39:02 -0700

    Add movement and rotation script to Derek scene

---
+++ 2DTopDownShooter/Assets/Scripts/DerekPlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DerekPlayerMovement : MonoBehaviour {
+
+    //GameObject player;
+    //SpriteRenderer playerRenderer;
+    //Rigidbody2D playerRB2D;
+
+    public int speed;
+    
+	// Use this for initialization
+	void Start () {
+        //// Get player
+        //player = GameObject.Find("DerekSprite");
+        //// Get player's SpriteRenderer component
+        //playerRenderer = player.GetComponent<SpriteRenderer>();
+        //playerRenderer.color = Color.red;
+        //// Get player's RigidBody2D component
+        ////playerRB2D = player.GetComponent<Rigidbody2D>();
+
+        speed = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Move sprite left/right
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+        // Move sprite up/down
+        transform.Translate(0, speed * Input.GetAxis("Vertical") * Time.deltaTime, 0);
+	}
+}
---
+++ 2DTopDownShooter/Assets/Scripts/DerekPlayerRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DerekPlayerRotation : MonoBehaviour {
+
+    public float mouseAngle = 0;
+    public float rotationSpeed = 5.0f;
+
+    private Vector2 mouseDirection;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mouseDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        mouseAngle = Mathf.Atan2(mouseDirection.y, mouseDirection.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, mouseAngle);
+    }
+}
